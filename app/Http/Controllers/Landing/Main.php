e32e8a4d667af57b8933c9a41c9b09dd9b593391
Mohid Meer <mohidfauji@gmail.com>
2024-04-01 11:18:40 +0500

    Added login via redirection

--- app/Http/Controllers/Landing/Main.php
+++ app/Http/Controllers/Landing/Main.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Landing;
 
 use App\Abstracts\Http\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Str;
+
 
 class Main extends Controller
 
@@ -21,25 +24,129 @@ class Main extends Controller
     }
     public function index(Request $request)
     {
-
-        if ($request->has('token')) {
-
-
-            $aaa=Crypt::encrypt('HI');
-            $d  =Crypt::decrypt($aaa);
-            $nodejsString = 'eyJpdiI6IjNCdVJlMkNySGhyRm45bmEybVVIS3c9PSIsInZhbHVlIjoieE9NelFZUXVHd3QvbHVYZ2tBZmZobzZFcVdXUVF1QVNJNGc3OFVMNmxmVT0iLCJtYWMiOiI5Y2VhN2YzMjgwNzc5ZjQ0NjBkMDI0MjYyYjBlZTFjYjUwOTVlYWQ2M2JhMGVkMjdiNjA2MzU2Mzc0MzUyOGRmIn0=';
-            $dnodejs=Crypt::decryptString($nodejsString);
-
-           return response([
-            'TEXT TO BE ENCRYPTED' => $d,
-            'ENCRYPTED BY LARAVEL' => $aaa,
-            'ENCRYPTED BY NODE JS' => $nodejsString,
-            'DECRYPTING THE LARAVEL STRING' =>$d,
-            'DECRYPTING THE NODEJS STRING' =>$dnodejs,
-           ],200);
-           
-
+        if (!$request->has('token')) {
+            return response()->json([
+                'status' => null,
+                'success' => false,
+                'error' => true,
+                'message' => 'No Token provided',
+                'data' => null,
+                'redirect' => null,
+            ]);
         }
-        // return response('dddd', 200);
+
+        $token = json_decode(Crypt::decryptString($request->token));
+
+
+        $tokenTimestamp = Carbon::createFromTimestampMs($token->timestamp);
+
+        $currentTimestamp = Carbon::now();
+
+        $timeDifferenceMinutes = $currentTimestamp->diffInMinutes($tokenTimestamp);
+
+        $timeDifferenceString = $tokenTimestamp->diffForHumans($currentTimestamp);
+
+        // dd([$tokenTimestamp,$currentTimestamp,$timeDifferenceMinutes]);
+
+        if ($timeDifferenceMinutes > 10) {
+            return response()->json([
+                'status' => null,
+                'success' => false,
+                'error' => true,
+                'message' => 'Token Expired ' . $timeDifferenceString,
+                'data' => null,
+                'redirect' => null,
+            ]);
+        }
+
+
+
+
+        // Attempt to login
+        if (!auth()->attempt([
+            "email" => $token->email,
+            "password" => $token->password,
+        ], false)) {
+            return response()->json([
+                'status' => null,
+                'success' => false,
+                'error' => true,
+                'message' => trans('auth.failed'),
+                'data' => null,
+                'redirect' => null,
+            ]);
+        }
+
+        // Get user object
+        $user = user();
+
+        // Setting Password In Session 
+        // Session::put('password', $request->password);
+
+
+        // Check if user is enabled
+        if (!$user->enabled) {
+            $this->logout();
+
+            return response()->json([
+                'status' => null,
+                'success' => false,
+                'error' => true,
+                'message' => trans('auth.disabled'),
+                'data' => null,
+                'redirect' => null,
+            ]);
+        }
+
+        $company = $user->withoutEvents(function () use ($user) {
+            return $user->companies()->enabled()->first();
+        });
+
+        // Logout if no company assigned
+        if (!$company) {
+            $this->logout();
+
+            return response()->json([
+                'status' => null,
+                'success' => false,
+                'error' => true,
+                'message' => trans('auth.error.no_company'),
+                'data' => null,
+                'redirect' => null,
+            ]);
+        }
+
+        // Redirect to portal if is customer
+        if ($user->isCustomer()) {
+            $path = session('url.intended', '');
+
+            // Path must start with company id and 'portal' prefix
+            if (!Str::startsWith($path, $company->id . '/portal')) {
+                $path = route('portal.dashboard', ['company_id' => $company->id]);
+            }
+
+            return response()->json([
+                'status' => null,
+                'success' => true,
+                'error' => false,
+                'message' => trans('auth.login_redirect'),
+                'data' => null,
+                'redirect' => url($path),
+            ]);
+        }
+
+        // Redirect to landing page if is user
+        $url = route($user->landing_page, ['company_id' => $company->id]);
+        // $url = route('chat-gpt.main.index', ['company_id' => $company->id]);
+
+        // return response()->json([
+        //     'status' => null,
+        //     'success' => true,
+        //     'error' => false,
+        //     'message' => trans('auth.login_redirect'),
+        //     'data' => null,
+        //     'redirect' => redirect()->intended($url)->getTargetUrl(),
+        // ]);
+        return redirect()->intended($url);
     }
 }
